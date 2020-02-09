4a526f5e2cc7ef0525bff3e90bd63c165f0822d1
Sergey Maksimenko <s.maxymenko@gmail.com>
2020-02-09 22:12:36 +0200

    rename languge param

--- src/lib/middleware/l10n.ts
+++ src/lib/middleware/l10n.ts
@@ -3,8 +3,8 @@ import { Request, Response, NextFunction } from "express";
 const l10nMDW = (req: Request, res: Response, next: NextFunction) => {
     const supportedLanguales: Array<String> = ["UA", "EN"];
     let lang: String = "UA";
-    if (req.query.lang && req.query.lang.length == 2) {
-        lang = req.query.lang;
+    if (req.query._lang && req.query._lang.length == 2) {
+        lang = req.query._lang;
     } else {
         if (req.cookies.selectedLanguage) {
             if (supportedLanguales.indexOf(req.cookies.selectedLanguage) > -1) {
